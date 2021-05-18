21d1a17a89bc6d3d78b8adf788d9dfe17a9d9ada
Nathaniel <NathanielKerr10@gmail.com>
2021-05-17 17:07:53 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,9 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @song.notes.build
+    @song.notes.build
+
   end
 
   def create
@@ -47,7 +50,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, notes_attributes: [:content])
   end
 end
-
